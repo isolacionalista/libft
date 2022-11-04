3519bb523be2cb812aac2665b3d5f02df15941db
Isis Mendonca <imendonc@c1r17s14.42lisboa.com>
2022-11-04 18:31:11 +0000

    lala

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: imendonc <imendonc@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/04 17:51:57 by imendonc          #+#    #+#             */
-/*   Updated: 2022/11/04 18:16:18 by imendonc         ###   ########.fr       */
+/*   Updated: 2022/11/04 18:30:31 by imendonc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,13 @@
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
 	char	*desti;
-	char	*temp;
 	char	*srci;
 
-	if (!src && !dest)
-		return (0);
-	if (dest == src)
-		return (dest);
 	desti = (char *)dest;
 	srci = (char *)src;
-	while (n--)
-	{
-		desti[n] = temp[n];
-		srci[n] = temp[n];
-	}
+	if (desti < srci)
+		desti[n] = srci[n];
+	else
+		ft_memcpy(desti, srci, n);
 	return (dest);
 }
