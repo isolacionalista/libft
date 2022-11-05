3c843bc8826eb3dcc32efd6f8408aef1ac899522
Isis Mendonca <imendonc@c1r17s14.42lisboa.com>
2022-11-05 16:16:36 +0000

    bzero

--- ft_bzero.c
+++ ft_bzero.c
@@ -6,13 +6,23 @@
 /*   By: imendonc <imendonc@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/05 15:46:43 by imendonc          #+#    #+#             */
-/*   Updated: 2022/11/05 15:47:22 by imendonc         ###   ########.fr       */
+/*   Updated: 2022/11/05 16:15:15 by imendonc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void ft_bzero(void *s, size_t n)
+void	ft_bzero(void *s, size_t n)
 {
-    
+	char	*zero;
+	size_t	i;
+
+	zero = (char *)s;
+	i = 0;
+	while (i < n)
+	{
+		zero[i] = 0;
+		i++;
+	}
+	zero = s;
 }
