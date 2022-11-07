c3504eb10291e7c853ef088774bb357576592f04
Isis Mendonca <imendonc@c1r17s8.42lisboa.com>
2022-11-07 14:35:55 +0000

    inicio part2

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: imendonc <imendonc@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/27 23:45:27 by imendonc          #+#    #+#             */
-/*   Updated: 2022/11/05 17:36:54 by imendonc         ###   ########.fr       */
+/*   Updated: 2022/11/07 13:48:09 by imendonc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,6 +59,11 @@ void		*ft_memmove(void *dest, const void *src, size_t n);
 void		*ft_memchr(const void *s, int c, size_t n);
 int			ft_memcmp(const void *s1, const void *s2, size_t n);
 
+//functions outside libc
+
 void		*ft_calloc(size_t nmemb, size_t size);
+void		ft_putchar_fd(char c, int fd);
+void		ft_putnbr_fd(int n, int fd);
+void		ft_putstr_fd(char *c, int fd);
 
 #endif
